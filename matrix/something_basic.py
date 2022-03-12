0cb4a3a75ee4687f76828c897aea623ff512727e
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2022-03-12 17:53:08 +0100

    well... turn TA07-11SRWA rows on and off

--- matrix/something_basic.py
+++ matrix/something_basic.py
@@ -2,11 +2,10 @@ from machine import Pin
 import utime
 
 rows = []
-
-print("--- STARTED ---")
+print("--- STARTED ---") # log
 
 status_led = Pin(25, Pin.OUT)
-status_led.value(1)
+status_led.value(1) # turn the litle green led on the pico board on
 
 rows.append(Pin(1, Pin.OUT))  # row1
 rows.append(Pin(2, Pin.OUT))  # row2
@@ -16,11 +15,11 @@ rows.append(Pin(5, Pin.OUT))  # row5
 
 for _ in range(12):
     for row in rows:
-        row.toggle()
-        utime.sleep(0.2)
-    print(str(_ + 1) + " : Finished")
+        row.toggle() # toggle row
+        utime.sleep(0.2) # sleep for 0.2sec
+    print(str(_ + 1) + " : Finished") # log
 
 for row in rows:
-    row.value(0)
-status_led.value(0)
-print("--- FINISHED ---")
+    row.value(0) # turn everything off incase its still on
+status_led.value(0) # turn the little green led off
+print("--- FINISHED ---") # log
